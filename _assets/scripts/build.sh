60f200d2b7c6049293911f80520ac4eeb8e87cb8
Vishal Neharkar <37950421+neharkarvishal@users.noreply.github.com>
2018-08-28 12:49:00 +0530

    •Code cleanup.

--- _assets/scripts/build.sh
+++ _assets/scripts/build.sh
@@ -33,3 +33,4 @@ curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CF_ZONE}/purge_cach
   -H "X-Auth-Key: ${CF_KEY}" \
   -H "Content-Type: application/json" \
   --data '{"purge_everything":true}'
+
